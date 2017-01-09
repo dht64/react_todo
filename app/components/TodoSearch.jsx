986abc216b81ddbb340584a44231016e1ee48653
Tung <tung@test.com>
2017-01-09 16:59:52 +0700

    Add Search component

---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+	handleSearch: function() {
+		var showCompleted = this.refs.showCompleted.checked;
+		var searchText = this.refs.searchText.value;
+
+		this.props.onSearch(showCompleted, searchText);
+	},
+	render: function() {
+		return (
+			<div>
+				<div>
+					<input type="search" ref="searchText" onChange={this.handleSearch} placeholder="Search todos"/>
+				</div>
+				<div>
+					<label>
+						<input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+						Show completed todos
+					</label>
+				</div>
+			</div>
+		);
+	}
+});
+
+module.exports = AddTodo;
