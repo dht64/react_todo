4d657ead7f30e64fc80d2e575e6e593a8041a6ae
Tung <tung@test.com>
2017-01-21 21:19:11 +0700

    Update todos on firebase

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -28,15 +28,12 @@ export var todosReducer = (state = [], action) => {
 				action.todo
 			];
 		// case for TOGGLE_TODO, !completed & update completedAt
-		case 'TOGGLE_TODO':
+		case 'UPDATE_TODO':
 			return state.map((todo) => {
 				if (todo.id === action.id) {
-					var nextCompleted = !todo.completed;
-
 					return {
 						...todo,
-						completed: nextCompleted,
-						completedAt: nextCompleted ? moment().unix() : undefined
+						...action.updates
 					};
 				} else {
 					return todo;
